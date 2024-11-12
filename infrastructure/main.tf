226b213b43cb8d94b999d39446b1883656c7edc0
Karis Omotosho <karis.omotosho@gmail.com>
2024-11-12 10:08:58 +0000

    Terraform infrastructure

---
+++ infrastructure/main.tf
@@ -0,0 +1,72 @@
+# Create an EC2 instance
+
+# Provider
+provider "aws" {
+
+  # Which region we use
+  region = "eu-west-1"
+}
+
+
+# CONTROLLER NODE
+
+
+# Security group
+resource "aws_security_group" "minikube_sg" {
+    name = var.sg_name
+  # Tags
+  tags = {
+    Name = var.sg_name
+
+  }
+
+}
+
+# NSG Rules
+resource "aws_vpc_security_group_ingress_rule" "allow_ssh_22" {
+
+  security_group_id = aws_security_group.minikube_sg.id
+  from_port         = 22
+  ip_protocol       = "tcp"
+  to_port           = 22
+  cidr_ipv4         = var.vpc_ssh_inbound_cidr
+  tags = {
+    Name = "Allow_SSH"
+  }
+}
+resource "aws_vpc_security_group_egress_rule" "allow_out_all" {
+
+  security_group_id = aws_security_group.minikube_sg.id
+  ip_protocol       = "All"
+  cidr_ipv4         = var.vpc_ssh_inbound_cidr
+  tags = {
+    Name = "Allow_Out_all"
+  }
+}
+
+
+# Resource to create
+resource "aws_instance" "minikube_instance" {
+
+  # AMI ID ami-0c1c30571d2dae5c9 (for ubuntu 22.04 lts)
+  ami = var.app_ami_id
+
+  instance_type = var.instance_type
+
+  # Public ip
+  associate_public_ip_address = var.associate_pub_ip
+
+  # Security group
+  vpc_security_group_ids = [aws_security_group.minikube_sg.id]
+
+  # SSH Key pair
+  key_name = var.ssh_key_name
+
+  # Name the resource
+  tags = {
+    Name = var.instance_name
+  }
+
+}
+
+
